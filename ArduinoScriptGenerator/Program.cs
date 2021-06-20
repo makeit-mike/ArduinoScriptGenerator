07f707d843b8bb1d4cd8d44147118dafa51e37f1
MakeIt-Mike <mike.isaac.jones@gmail.com>
2021-06-20 17:25:43 -0500

    Making the Script only 1 line for easier portability in the console.
    Adding method to search for the Arduino's COM Port.
    
    Adding method to actually invoke the powershell script.

--- ArduinoScriptGenerator/Program.cs
+++ ArduinoScriptGenerator/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Management;
+using System.Management.Automation;
+using System.Timers;
 
 namespace ArduinoScriptGenerator
 {
@@ -15,11 +22,11 @@ namespace ArduinoScriptGenerator
                 {
                     BuildScript();
 
-                    Console.WriteLine("Do you want to continue? (Y/n)");
+                    Console.WriteLine("\r\nDo you want to continue? (Y/n)");
                     shouldContinue = Console.ReadLine() != "n";
                 }
             }
-            catch (Exception ex) {ErrorCatch(ex);}
+            catch (Exception ex) { ErrorCatch(ex); }
         }
         private static void BuildScript()
         {
@@ -27,25 +34,90 @@ namespace ArduinoScriptGenerator
             {
                 string filePath = GetFilePath();
                 int indexOfHexFile = filePath.LastIndexOf('\\'); //"C:\someFolders\unimap.hex" where \ before unimap is the last index before the hex file.
-                var pathParts = new[]
+                
+                //var pathParts = new[]     //Use if you want to split up the location of the HEX and the actual HEX file.
+                //{
+                //    filePath.Substring(0, indexOfHexFile),
+                //    filePath.Substring(indexOfHexFile)
+                //};
+
+                string comPort = DetermineCOMPort();
+
+                Console.WriteLine("\r\nCommand-Line Script Generated.\r\nCopy and paste into a terminal of your choice.\r\nRun the AVRDude script with Bootloader mode ON.\r\n");
+
+                //Console.WriteLine($"cd {pathParts[0]}"); //Looks like: "C:\someFolders"
+                //Console.WriteLine($"avrdude -patmega32u4 -cavr109 -b57600 -Uflash:w:{pathParts[1]} -P{comPort}"); //Looks like: "avrdude -patmega32u4 -cavr109 -b57600 -Uflash:w:unimap.hex -PCOM17" 
+
+                string powerShellScript = $"avrdude -patmega32u4 -cavr109 -b57600 -Uflash:w:\"{filePath}\":a -P{comPort}";
+                Console.WriteLine(powerShellScript);
+
+                Console.WriteLine("Would you like to flash file now? (Y/n)");
+                bool shouldAttemptFlash = Console.ReadLine() == "Y";
+                if (shouldAttemptFlash)
                 {
-                    filePath.Substring(0, indexOfHexFile),
-                    filePath.Substring(indexOfHexFile)
-                };
-
-                //Looks like: "C:\someFolders"
-                Console.WriteLine($"cd {pathParts[0]}");
-                //Looks like: "avrdude -patmega32u4 -cavr109 -b57600 -Uflash:w:unimap.hex -PCOM17" //Will need to manually set your port number
-                Console.WriteLine($"avrdude -patmega32u4 -cavr109 -b57600 -Uflash:w:{pathParts[1]} -PCOM17");
-
+                    RunPowershellScript(powerShellScript);
+                }
             }
             catch (Exception ex) { ErrorCatch(ex); }
         }
+
+        private static string DetermineCOMPort()
+        {
+            Console.WriteLine("\r\nWould you like to search for an Arduino COM Port? (Y/n)");
+            string comPort = "";
+
+            if (Console.ReadLine() == "Y")
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+
+                Console.WriteLine("Activate Arduino Bootloader. . .");
+                int numSecondsToSearchForComPort = 5;
+                while (comPort.Length == 0 && stopWatch.ElapsedMilliseconds < numSecondsToSearchForComPort * 1000)
+                {
+                    comPort = AutodetectArduinoPort();
+                }
+                stopWatch.Stop();
+            }
+            if (comPort.Length == 0)
+            {
+                comPort = "COM";
+                Console.WriteLine("\r\nCOM Port not found. You will need to manually set your port number. Can be found in the Device Manager or in the Arduino IDE.\r\n");
+            }
+
+            return comPort;
+        }
+        private static void RunPowershellScript(string powerShellScript)
+        {
+            try
+            {
+                using (PowerShell powerShell = PowerShell.Create())
+                {
+                    // Source functions.
+                    powerShell.AddScript(powerShellScript);
+
+                    // invoke execution on the pipeline (collecting output)
+                    Console.WriteLine("Invoking script in PowerShell");
+                    Collection<PSObject> PSOutput = powerShell.Invoke();
+
+                    // loop through each output object item
+                    foreach (PSObject outputItem in PSOutput)
+                        if (outputItem != null) // if null object was dumped to the pipeline during the script then a null object may be present here
+                            Console.WriteLine($"$ [{outputItem}]");
+
+                    if (powerShell.Streams.Error.Count > 0) // check the other output streams (for example, the error stream)
+                        foreach (var err in powerShell.Streams.Error)
+                            Console.WriteLine($"$ [{err.Exception.Message}]");
+                }
+            }
+            catch (Exception ex) { ErrorCatch(ex); }
+        }
+
         private static void ErrorCatch(Exception ex)
         {
             Console.WriteLine(ex.Message);
 
-            Console.WriteLine($"Would you like to see verbose error details? (Y/n)");
+            Console.WriteLine($"\r\nWould you like to see verbose error details? (Y/n)");
             if (Console.ReadLine() == "Y")
             {
                 Console.WriteLine(ex.StackTrace);
@@ -57,13 +129,13 @@ namespace ArduinoScriptGenerator
             Console.WriteLine("Paste path to HEX file.");
 
             var filePath = Console.ReadLine();
-            Console.WriteLine($"Is this path correct (Y/n): {filePath}");
+            Console.WriteLine($"\r\nIs this path correct (Y/n): {filePath}");
 
             bool isCorrectPath = Console.ReadLine() == "Y";
 
             if (isCorrectPath && filePath.Length > 0)
             {
-                return filePath.Trim().Replace("\"","");
+                return filePath.Trim().Replace("\"", "");
             }
             else
             {
@@ -72,5 +144,42 @@ namespace ArduinoScriptGenerator
                 return filePath;
             }
         }
+
+        private static string AutodetectArduinoPort()
+        {
+            ManagementScope connectionScope = new ManagementScope();
+            SelectQuery serialQuery = new SelectQuery("SELECT * FROM Win32_SerialPort");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(connectionScope, serialQuery);
+
+            try
+            {
+                foreach (ManagementObject item in searcher.Get())
+                {
+                    string desc = item["Description"].ToString();
+                    string deviceId = item["DeviceID"].ToString();
+
+                    if (desc.Contains("Arduino"))
+                    {
+                        Console.WriteLine($"\r\nArduino COM Port Found at {deviceId}!\r\n");
+                        return deviceId;
+                    }
+                }
+            }
+            catch (ManagementException e) { }
+            return "";
+        }
     }
+}
+
+class USBDeviceInfo
+{
+    public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+    {
+        this.DeviceID = deviceID;
+        this.PnpDeviceID = pnpDeviceID;
+        this.Description = description;
+    }
+    public string DeviceID { get; private set; }
+    public string PnpDeviceID { get; private set; }
+    public string Description { get; private set; }
 }
